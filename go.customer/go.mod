be1292bb6d5277f3434e90b65c12a0857a5f95e8
diego3 <www.diegosantos.com.br@gmail.com>
2021-08-01 22:08:01 -0300

    code comment

--- go.customer/go.mod
+++ go.customer/go.mod
@@ -3,4 +3,8 @@ module go.customer
 go 1.15
 
 require github.com/gorilla/mux v1.8.0
-require github.com/google/uuid v1.3.0
+
+require (
+	github.com/go-redis/redis/v8 v8.11.1 // indirect
+	github.com/google/uuid v1.3.0
+)
--- go.customer/main.go
+++ go.customer/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
 	"go.customer/controller"
 )
@@ -22,6 +24,11 @@ func handleRequests() {
 
 func init() {
 	log.Println("Customer service init")
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	redisClient.LPush(context.Background(), "redis-key-1", "customer1")
+	log.Println(redisClient)
 }
 
 func main() {
